136ff83f2e3d3d763736e1496becd7bb73c83db3
Aleksey Drobnych <adrobnych@gmail.com>
2014-05-28 12:52:42 +0300

    POX admin test green

--- rest_0_1_levels/POX/src/main/java/com/sysiq/restwebinars/restexchange1/resources/SetRateResource.java
+++ rest_0_1_levels/POX/src/main/java/com/sysiq/restwebinars/restexchange1/resources/SetRateResource.java
@@ -1,5 +1,23 @@
 package com.sysiq.restwebinars.restexchange1.resources;
 
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/setrate")
 public class SetRateResource {
 
+    @POST 
+    @Consumes(MediaType.APPLICATION_XML)
+    public Response setRate(String xmlString) {
+    	 
+		System.out.println("____________________ request body: __________________________");
+		System.out.println(xmlString);
+		
+		return Response.status(204).build();
+ 
+	}
 }
