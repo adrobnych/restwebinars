28ea67d000fb7bfd9a90d29624d9da78a3d809ef
Aleksey Drobnych <adrobnych@gmail.com>
2014-05-29 11:40:24 +0300

    URI template user test red

---
+++ rest_0_1_levels/POX/src/test/java/com/sysiq/restwebinars/restexchange1/UserTest.java
@@ -0,0 +1,40 @@
+package com.sysiq.restwebinars.restexchange1;
+
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.core.header.MediaTypes;
+import com.sun.jersey.test.framework.JerseyTest;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class UserTest extends JerseyTest {
+
+    public UserTest()throws Exception {
+        super("com.sysiq.restwebinars.restexchange1.resources");
+    }
+
+
+    @Test
+    public void testSetAndGetRate() {
+        WebResource webResource = resource();
+        
+        String input = 	"<setrate xmlns=\"http://schemas.restexchange.com/setrate\">"+
+        					"<items>"+
+        						"<item>"+
+        							"<from>USD</from>"+
+        							"<to>UAH</to>"+
+        							"<factor>12.55</factor>"+
+        						"</item>"+
+        					"</items>"+
+        				"</setrate>";
+        
+		ClientResponse response = webResource.path("setrate").type("application/xml").post(ClientResponse.class, input);
+
+        assertEquals(204, response.getStatus());
+        
+        String responseConvertMoney = webResource.path("amount_of_uah_from/100/usd").get(String.class);
+        assertEquals("1255", responseConvertMoney);
+    }
+
+}
